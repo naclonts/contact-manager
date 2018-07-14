8b8d03675ded700a6007fd06bc3a3ced172d1103
kokopelli314 <nacclonts@gmail.com>
2018-07-13 23:59:47 -0600

    clean up model update function

--- app/controllers.py
+++ app/controllers.py
@@ -49,7 +49,6 @@ def get_contacts(user):
     return jsonify([model_to_dict(c) for c in contacts])
 
 def add_contact(user, c):
-    # contact = Contact(first_name=c['first_name'], last_name=c['last_name'])
     contact = Contact(**c) # unpack dict `c` to model class parameters
     user.contacts.append(contact)
     db.session.commit()
@@ -62,20 +61,12 @@ def delete_contact(c):
 
 
 def update_contact(contact_dict):
-    # Prevent key errors when accessing fields
-    def key_or_none(key, dic):
-        if key in dic:
-            return dic[key]
-        else:
-            return None
-    # Assign JSON fields to model
     contact = Contact.query.get(contact_dict['id'])
-    contact.first_name = key_or_none('first_name', contact_dict)
-    contact.last_name = key_or_none('last_name', contact_dict)
-    contact.date_of_birth = key_or_none('date_of_birth', contact_dict)
-    contact.addresses = key_or_none('addresses', contact_dict)
-    contact.phone_numbers = key_or_none('phone_numbers', contact_dict)
-    contact.emails = key_or_none('emails', contact_dict)
+
+    # Assign JSON fields to model
+    for key, val in contact_dict.items():
+        setattr(contact, key, val)
+
     # Save the changes
     db.session.commit()
 
