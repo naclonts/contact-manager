15332cecf8212365a7b99b6c66d95613edd9e70f
kokopelli314 <nacclonts@gmail.com>
2018-07-14 23:30:27 -0600

    add 404 page

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,9 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask.json import JSONEncoder
+from datetime import date
 from dotenv import load_dotenv
 
 # Load environment variables file
@@ -14,10 +16,25 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
-# Sample HTTP error handling
-# @app.errorhandler(404)
-# def not_found(error):
-#     return render_template('404.html'), 404
+
+# Define JSON encoding class to encode dates in ISO format
+class JSONEncoderISODate(JSONEncoder):
+    def default(self, obj):
+        try:
+            if isinstance(obj, date):
+                return obj.isoformat()
+            iterable = iter(obj)
+        except TypeError:
+            pass
+        else:
+            return list(iterable)
+        return JSONEncoder.default(self, obj)
+app.json_encoder = JSONEncoderISODate
+
+# Not found page
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html'), 404
 
 from app.controllers import router
 app.register_blueprint(router)
